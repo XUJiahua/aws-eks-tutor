14a72878fbca8d5a9ebc93f7ee9061b842086479
jiahua <jiahua.xu@outlook.com>
2020-11-18 17:18:06 +0800

    install aws lb controller

--- setup/awslb.sh
+++ setup/awslb.sh
@@ -1,32 +1,33 @@
 eksctl utils associate-iam-oidc-provider \
     --region ap-northeast-1 \
-    --cluster eks-try-2020-1 \
+    --cluster eks-try-2020-2 \
     --approve
 
+curl -o iam-policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json
+
 aws iam create-policy \
-    --policy-name AWSLoadBalancerControllerIAMPolicy-1 \
+    --policy-name AWSLoadBalancerControllerIAMPolicy-2 \
     --policy-document file://iam-policy.json
 
 # {
 #     "Policy": {
-#         "PolicyName": "AWSLoadBalancerControllerIAMPolicy-1",
-#         "PolicyId": "ANPARPKBYTMYRXJJLJKWF",
-#         "Arn": "arn:aws:iam::101606529841:policy/AWSLoadBalancerControllerIAMPolicy-1",
+#         "PolicyName": "AWSLoadBalancerControllerIAMPolicy-2",
+#         "PolicyId": "ANPARPKBYTMYVV2D3PMYP",
+#         "Arn": "arn:aws:iam::101606529841:policy/AWSLoadBalancerControllerIAMPolicy-2",
 #         "Path": "/",
 #         "DefaultVersionId": "v1",
 #         "AttachmentCount": 0,
 #         "PermissionsBoundaryUsageCount": 0,
 #         "IsAttachable": true,
-#         "CreateDate": "2020-11-14T06:43:04+00:00",
-#         "UpdateDate": "2020-11-14T06:43:04+00:00"
+#         "CreateDate": "2020-11-18T09:10:19+00:00",
+#         "UpdateDate": "2020-11-18T09:10:19+00:00"
 #     }
 # }
 
-
 eksctl create iamserviceaccount \
-  --cluster=eks-try-2020-1 \
+  --cluster=eks-try-2020-2 \
   --namespace=kube-system \
   --name=aws-load-balancer-controller \
-  --attach-policy-arn=arn:aws:iam::101606529841:policy/AWSLoadBalancerControllerIAMPolicy-1 \
+  --attach-policy-arn=arn:aws:iam::101606529841:policy/AWSLoadBalancerControllerIAMPolicy-2 \
   --override-existing-serviceaccounts \
   --approve
